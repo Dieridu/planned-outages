7191cb672c679ec256a9734724d15d0c614abacd
Monika Kidyba <kidyba.m@gmail.com>
2025-05-29 15:49:53 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,2 +1,21 @@
+// ===== server/db.js =====
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./outages.db');
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS outages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    area TEXT,
+    date TEXT,
+    reason TEXT
+  )`);
+});
+
+module.exports = {
+  all: (sql) => new Promise((resolve, reject) => {
+    db.all(sql, [], (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  })
+};
